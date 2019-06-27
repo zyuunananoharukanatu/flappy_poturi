e7f4d8cc9a86aab3a9af0674bbb899c67e760fee
takahasinaohiro <tk@takahasinaohironoMacBook-Pro.local>
2019-06-27 23:11:21 +0900

    ジャンプゲージを縦に　あと諸々修正

--- flappy_poturi.py
+++ flappy_poturi.py
@@ -24,8 +24,8 @@ class App:
         # オブジェクト関連
         self.upper_star = [(-10, 75), (40, 65), (90, 60)]
         self.downer_star = [(10, 25), (70, 35), (120, 15)]
-        self.floor = [(i * 60, randint(8, 104), True) for i in range(4)]
-        self.fruit = [(i * 60, randint(0, 104), randint(0, 2), True) for i in range(4)]
+        self.obstacle = [(i * 60, randint(8, 104), True) for i in range(4)]
+        self.water = [(i * 60, randint(0, 104), randint(0, 2), True) for i in range(4)]
 
         pyxel.playm(0, loop=True)
 
@@ -39,11 +39,11 @@ class App:
 
         pyxel.rect(100,100,130,130,12)
 
-        for i, v in enumerate(self.floor):
-            self.floor[i] = self.update_floor(*v)
+        for i, v in enumerate(self.obstacle):
+            self.obstacle[i] = self.update_obstacle(*v)
 
-        for i, v in enumerate(self.fruit):
-            self.fruit[i] = self.update_fruit(*v)
+        for i, v in enumerate(self.water):
+            self.water[i] = self.update_water(*v)
 
     def update_player(self):
         # spaceでjump
@@ -53,7 +53,7 @@ class App:
                 self.jump_start_time = pyxel.frame_count
                 self.jump_gauge -= 1
 
-            elif (pyxel.frame_count - self.jump_start_time) > 10:
+            elif (pyxel.frame_count - self.jump_start_time) > 7:
                 self.jump_gauge -= 1
                 self.jump_start_time = pyxel.frame_count
 
@@ -74,7 +74,7 @@ class App:
                 self.player_vy = 0
                 self.player_is_alive = True
 
-    def update_floor(self, x, y, is_active):
+    def update_obstacle(self, x, y, is_active):
         if is_active:
             if (
                 self.player_x + 14 >= x
@@ -100,7 +100,7 @@ class App:
 
         return (x, y, is_active)
 
-    def update_fruit(self, x, y, kind, is_active):
+    def update_water(self, x, y, kind, is_active):
         if is_active and abs(x - self.player_x) < 12 and abs(y - self.player_y) < 12:
             is_active = False
             pyxel.play(3, 4)
@@ -144,11 +144,11 @@ class App:
                 pyxel.blt(x + i * 160 - offset, y, 0, 0, 31, 56, 12, 12)
 
         # draw obstacle
-        for x, y, is_active in self.floor:
+        for x, y, is_active in self.obstacle:
             pyxel.blt(x, y, 0, 0, 16, 8, 8, 12)
 
-        # draw fruits
-        for x, y, kind, is_active in self.fruit:
+        # draw water
+        for x, y, kind, is_active in self.water:
             if is_active:
                 pyxel.blt(x, y, 0, 32 + kind * 16, 0, 16, 16, 11)
 
@@ -170,10 +170,10 @@ class App:
         pyxel.text(4, 4, s, 7)
 
         for i in range(0, self.jump_gauge):
-            pyxel.rect(4 + i * 3, 16, 3 + (i + 1) * 3, 20, 12)
+            pyxel.rect(4, -3 * i + 68, 8, -3 * i + 71, 12)
         
         for i in range(self.jump_gauge, 20):
-            pyxel.rectb(4 + i * 3, 16, 3 + (i + 1) * 3, 20, 12)
+            pyxel.rectb(4, -3 * i + 68, 8, -3 * i + 71, 12)
 
 
 App()
